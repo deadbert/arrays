390ba68e1b3e5fcbd01723f13283fdcff292662d
Nick Spencer <strangebrew202@gmail.com>
2023-06-19 17:58:40 -0600

    Perform 3 array methods

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,15 @@ names_array = ["Nick", "Lily", "Bryden", "Phil"]
 ages_array = [34, 4, 40, 38]
 accounts_array = [0.0, 0.0, 100000.0, 1000.1]
 success_array = [false, true, true, true]
+
+#pop out and return the last element from names_array
+names_array.pop
+
+#push 2 new elements appended to the end of ages_array
+ages_array.push(56, 75)
+
+#shift, remove and return the first element of accounts_array
+accounts_array.shift
+
+#unshift, append 1 new element to the front of success_array
+success_array.shift(true)
